2837980ca3e1253a5da67a5fd5769e3e2f276f8f
Pierre Bockner <pierre.bockner@gmail.com>
2020-05-24 19:43:45 +0200

    Desensitive from light sources, Add high and low alert levels to differ from image noisyness

--- main.cpp
+++ main.cpp
@@ -26,6 +26,7 @@ using namespace cv;
 /*
  * Todo: Record a video, on trigger, store and/or send via mail.
  * Use colour room which is not as light sensitive
+ * Use high quality image for Email
  */
 
 namespace
@@ -35,7 +36,8 @@ namespace
 	constexpr int VK_ESCAPE{27};
 	constexpr auto TRIGGER_DELAY_SECONDS = std::chrono::seconds(90);
 	constexpr double TRIGGER_SATURATION{0.020};
-	constexpr unsigned int TRIGGER_ON_CONSECUTIVE_FRAME{FPS*3};
+	constexpr unsigned int MINOR_TRIGGER_ON_CONSECUTIVE_FRAME{FPS*3};
+	constexpr unsigned int MAJOR_TRIGGER_ON_CONSECUTIVE_FRAME{FPS*10};
 	
 	
 	constexpr char ARG_URL[] = "--url";
@@ -48,8 +50,15 @@ namespace
 	constexpr unsigned int CAPTURE_ERROR_REINITIALIZE{100};
 }
 
+enum AlertLevel
+{
+	LOW = 0,
+	MIDDLE,
+	HIGH
+};
+
 std::list<std::string> persistImages(Persistence& persistence, std::list<cv::Mat*> images, char isoTime[ISO_TIME_BUFFER_SIZE], const std::time_t& triggerTime);
-void sendMail(const std::string& script, const std::string& email, char isoTime[ISO_TIME_BUFFER_SIZE], 
+void sendMail(const AlertLevel level, const std::string& script, const std::string& email, char isoTime[ISO_TIME_BUFFER_SIZE], 
 		const std::time_t& triggerTime, const float saturation, float fps, 
 		const Persistence& storage, std::list<std::string> attachmentFiles);
 
@@ -112,21 +121,23 @@ int main(int argc, char** argv)
 		cv::namedWindow("ForegroundSmoothed", cv::WINDOW_AUTOSIZE);	
 	}
 	
-    	cv::Ptr<cv::BackgroundSubtractor> backgroundSubtractor = createBackgroundSubtractorMOG2(FPS * 60 * 5, 32, true);
+    	cv::Ptr<cv::BackgroundSubtractor> backgroundSubtractor = createBackgroundSubtractorMOG2(FPS * 60 * 2, 32, true);
     	//cv::Ptr<cv::BackgroundSubtractor> backgroundSubtractor = createBackgroundSubtractorKNN(FPS * 60 * 5, 100, true);
     	if (!backgroundSubtractor)
     	{
 		std::cerr << "Could not create backgound subtractor..." << std::endl;
 		return -1;    
     	}
-	std::chrono::system_clock::time_point trigger = std::chrono::system_clock::now();
+	std::chrono::system_clock::time_point minorTrigger = std::chrono::system_clock::now();
+	std::chrono::system_clock::time_point majorTrigger = std::chrono::system_clock::now();
 	std::chrono::system_clock::time_point currentTime;
 	std::chrono::system_clock::time_point fpsTimer = std::chrono::system_clock::now();
 	
 	
 	// Set location for videos & images to be stored.
 	Persistence persistence(storage);
-	IntrusionTrigger intrusionTrigger(TRIGGER_ON_CONSECUTIVE_FRAME, TRIGGER_SATURATION);
+	IntrusionTrigger minorIntrusionTrigger(MINOR_TRIGGER_ON_CONSECUTIVE_FRAME, TRIGGER_SATURATION);
+	IntrusionTrigger majorIntrusionTrigger(MAJOR_TRIGGER_ON_CONSECUTIVE_FRAME, TRIGGER_SATURATION);
 	bool running{true};	
 	char isoTime[ISO_TIME_BUFFER_SIZE] = {0};		
 	cv::Mat frame, frameSmoothed, foregroundMask, foregroundMaskSmoothed;
@@ -163,6 +174,14 @@ int main(int argc, char** argv)
 	 	
 		// Create Background from noise reduced image
 		cv::blur(frame, frameSmoothed, cv::Size(3,3));
+		// Remove light sensitive reactions
+		cv::cvtColor(frameSmoothed, frameSmoothed, cv::COLOR_RGB2YUV);
+		std::vector<cv::Mat> channels;
+		cv::split(frameSmoothed, channels);
+		cv::equalizeHist(channels[0], channels[0]);
+		cv::merge(channels, frameSmoothed);
+		cv::cvtColor(frameSmoothed, frameSmoothed, CV_YUV2RGB);
+		
         	backgroundSubtractor->apply(frameSmoothed, foregroundMask);
 		if (visual) cv::imshow("Foreground", foregroundMask);
 		
@@ -173,7 +192,8 @@ int main(int argc, char** argv)
 		if (visual) cv::imshow("ForegroundSmoothed", foregroundMaskSmoothed);
 		
 		// Evaluate image foreground saturation 
-		double saturation = intrusionTrigger.update(foregroundMaskSmoothed);
+		double minorSaturation = minorIntrusionTrigger.update(foregroundMaskSmoothed);
+		double majorSaturation = majorIntrusionTrigger.update(foregroundMaskSmoothed);
 				
 		currentTime = std::chrono::system_clock::now();
 
@@ -189,11 +209,12 @@ int main(int argc, char** argv)
 		++frames;
 
 		// check, if movement was detected
-		if (intrusionTrigger.hasTriggered() && (currentTime > trigger + TRIGGER_DELAY_SECONDS))
+				
+		if (majorIntrusionTrigger.hasTriggered() && (currentTime > majorTrigger + TRIGGER_DELAY_SECONDS))
 		{
 			// Reset timer for next trigger condition
-			trigger = std::chrono::system_clock::now();
-			std::time_t cTrigger = std::chrono::system_clock::to_time_t(trigger);
+			majorTrigger = std::chrono::system_clock::now();
+			std::time_t cTrigger = std::chrono::system_clock::to_time_t(majorTrigger);
 			std::cout << "Possible Intrusion: " << std::ctime(&cTrigger) << std::endl;								
 				
 			// Save triggering images fullfilling saturation condition.
@@ -201,9 +222,24 @@ int main(int argc, char** argv)
 			std::list<std::string> persistedImages = persistImages(persistence, imageList, isoTime, cTrigger);
 			
 			//send mail
-			sendMail(scripts, email, isoTime, cTrigger, saturation, fps, persistence, persistedImages);
-	
+			sendMail(AlertLevel::HIGH, scripts, email, isoTime, cTrigger, minorSaturation, fps, persistence, persistedImages);	
 		}
+		else if (minorIntrusionTrigger.hasTriggered() && (currentTime > minorTrigger + TRIGGER_DELAY_SECONDS))
+		{
+			// Reset timer for next trigger condition
+			minorTrigger = std::chrono::system_clock::now();
+			std::time_t cTrigger = std::chrono::system_clock::to_time_t(minorTrigger);
+			std::cout << "Possible Intrusion: " << std::ctime(&cTrigger) << std::endl;								
+				
+			// Save triggering images fullfilling saturation condition.
+			std::list<cv::Mat*> imageList{&frame, &foregroundMask};
+			std::list<std::string> persistedImages = persistImages(persistence, imageList, isoTime, cTrigger);
+			
+			//send mail
+			sendMail(AlertLevel::LOW, scripts, email, isoTime, cTrigger, minorSaturation, fps, persistence, persistedImages);	
+		}
+
+		
 		if (visual)
 		{
 			// Wait and Evaluate pressed keys.
@@ -249,7 +285,7 @@ std::list<std::string> persistImages(Persistence& persistence, std::list<cv::Mat
 	return persisted;
 }
 
-void sendMail(const std::string& script, const std::string& email, char isoTime[ISO_TIME_BUFFER_SIZE], 
+void sendMail(const AlertLevel level, const std::string& script, const std::string& email, char isoTime[ISO_TIME_BUFFER_SIZE], 
 		const std::time_t& triggerTime, const float saturation, float fps, 
 		const Persistence& storage, std::list<std::string> attachmentFiles)
 {	
@@ -258,7 +294,14 @@ void sendMail(const std::string& script, const std::string& email, char isoTime[
 	std::stringstream compose;	
 
 	// Subject, currently colons are erronous during image creation.
-	compose << "Intrusion detected " << isoTime;
+	if (level == AlertLevel::HIGH)
+	{	
+		compose << "HIGH Alert: Intrusion detected " << isoTime;
+	}
+	else
+	{
+		compose << "LOW Alert: Disturbtion detected " << isoTime;	
+	}
 	std::string subject = compose.str();
 	
 	// Body
